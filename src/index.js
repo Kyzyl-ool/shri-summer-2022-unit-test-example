6f8d60cf92daec6443939698ea38a4874edf6dbc
k.kyzyl-ool <kyzyloolk@mail.ru>
2022-08-16 21:54:14 +0300

    d1 tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-export const testFunction = (a, b) => a + b;
+export const innTest = (value) => {
+  const type = typeof value
+  if (!['string', 'number'].includes(type)) {
+    return false;
+  }
+
+  if (type === 'number') {
+    value = String(value);
+  }
+
+  const {length} = value;
+  if (length === 10) {
+    const weights = [2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
+    let sum = 0;
+    for (let i = 0; i < length; i++) {
+      sum += +value[i] * weights[i];
+    }
+    sum %= 11;
+    if (sum > 9) {
+      sum %= 10;
+    }
+
+    return +value[length - 1] === sum;
+  }
+
+  if (length === 12) {
+    const weights1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
+    let sum1 = 0;
+    for (let i = 0; i < 11; i++) {
+      sum1 += +value[i] * weights1[i];
+    }
+    sum1 %= 11;
+    if (sum1 > 9) {
+      sum1 %= 10;
+    }
+
+    const weights2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8, 0];
+    let sum2 = 0;
+    for (let i = 0; i < 12; i++) {
+      sum2 += +value[i] * weights2[i];
+    }
+    sum2 %= 11;
+    if (sum2 > 9) {
+      sum2 %= 10;
+    }
+
+
+    return sum1 === +value[10] && sum2 === +value[11];
+  }
+
+  return false;
+}
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,50 @@
-import {testFunction} from '.';
+import {innTest} from '.';
 
-describe('Тестовая функция, для наглядного примера', () => {
-    it('Складывает два числа вместе', () => {
-        expect(testFunction(2, 3)).toBe(5);
+describe('Функция innTest', () => {
+    it('Валидный числовой ИНН ФЛ', () => {
+        expect(innTest(895502613880)).toBeTruthy();
     });
+
+    it('Невалидный числовой ИНН ФЛ', () => {
+        expect(innTest(895502613881)).toBeFalsy();
+    });
+
+    it('Валидный строчный ИНН ФЛ', () => {
+        expect(innTest('895502613880')).toBeTruthy();
+    });
+
+    it('Невалидный строчный ИНН ФЛ', () => {
+        expect(innTest('895502613881')).toBeFalsy();
+    });
+
+    it('Валидный числовой ИНН ЮЛ', () => {
+        expect(innTest(2312271585)).toBeTruthy();
+    });
+
+    it('Невалидный числовой ИНН ЮЛ', () => {
+        expect(innTest(2312271586)).toBeFalsy();
+    });
+
+    it('Валидный строчный ИНН ЮЛ', () => {
+        expect(innTest('2312271585')).toBeTruthy();
+    });
+
+    it('Невалидный строчный ИНН ЮЛ', () => {
+        expect(innTest('2312271586')).toBeFalsy();
+    });
+
+    it('Объект', () => {
+        expect(innTest({})).toBeFalsy();
+    });
+
+    it('Длина не 10 и не 12', () => {
+        expect(innTest('2312271585111')).toBeFalsy();
+        expect(innTest('2312271')).toBeFalsy();
+        expect(innTest(2312271585111)).toBeFalsy();
+        expect(innTest(2312271)).toBeFalsy();
+    });
+
+    it('Валидный ИНН в 16ричном формате', () => {
+        expect(innTest(0x89D276E1)).toBeTruthy();
+    })
 });
